b1063aff2260374ad16c00feaceba982269b85da
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-27 00:35:28 +0900

    [junu0516/airbnb#11] Feat: ConditionSettingViewController의 뷰 하단에 ConditionSettingTableView(검색 조건 테이블 뷰) 서브 뷰로 추가

--- iOS/Airbnb/ConditionSettingViewController.swift
+++ iOS/Airbnb/ConditionSettingViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+
 final class ConditionSettingViewController: UIViewController {
     
     private lazy var dummyView: UIView = {
@@ -7,18 +8,50 @@ final class ConditionSettingViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    private lazy var conditionSettingTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isScrollEnabled = false
+        tableView.isUserInteractionEnabled = false
+        tableView.register(ConditionSettingTableViewCell.self, forCellReuseIdentifier: ConditionSettingTableViewCell.identifier)
+        return tableView
+    }()
+    
+    private let categories: [String] = ["위치", "체크인/체크아웃", "요금", "인원"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.title = "숙소 찾기"
         
         self.view.addSubview(dummyView)
+        self.view.addSubview(conditionSettingTableView)
         
         NSLayoutConstraint.activate([
             dummyView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             dummyView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
             dummyView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
             dummyView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.6),
+            
+            conditionSettingTableView.topAnchor.constraint(equalTo: dummyView.bottomAnchor),
+            conditionSettingTableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            conditionSettingTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            conditionSettingTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+extension ConditionSettingViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ConditionSettingTableViewCell.identifier, for: indexPath) as? ConditionSettingTableViewCell else { return UITableViewCell() }
+        cell.updateLabelText(conditionTitle: ConditionCategory.allCases[indexPath.row].description, conditionValue: "")
+        return cell
     }
 }
