e727a25ba364b01c2436aaaa2262a1744c58bd3a
Jinsujin <sylpid003@gmail.com>
2022-05-30 17:34:01 +0900

    Env: googlemap 키 환경변수를 plist 로 변경

--- iOS/Airbnb/Supportings/SceneDelegate.swift
+++ iOS/Airbnb/Supportings/SceneDelegate.swift
@@ -8,8 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        GMSServices.provideAPIKey(PrivateStorage.mapApiKey)
-
+        GMSServices.provideAPIKey(PrivateStorage.apiKey)  
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = UINavigationController(rootViewController: HomeViewController())
         window.makeKeyAndVisible()
