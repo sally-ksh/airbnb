a1c6025fdb3f0bab1bba52230ea6eb528dac8776
Jinsujin <sylpid003@gmail.com>
2022-05-30 21:48:54 +0900

    [junu0516/airbnb#24] Feat: 객체를 Data 로 인코딩하는 함수 구현

--- iOS/Airbnb/Server/AccomodationDetailRepository.swift
+++ iOS/Airbnb/Server/AccomodationDetailRepository.swift
@@ -8,6 +8,10 @@ struct JsonHandler {
     func convertJsonToObject<T: Decodable>(from data: Data, to targetType: T.Type) -> T? {
         return try? decoder.decode(T.self, from: data)
     }
+    
+    func convertObjectToJson<T: Encodable>(from object: T) -> Data? {
+        return try? encoder.encode(object)
+    }
 }
 
 
@@ -18,7 +22,7 @@ struct AccomodationDetailRepository {
     
     
     func fetch(roomId: UniqueID, completion: @escaping (RoomDetail) -> Void) {
-        NetworkHandler().request(endPoint: .accomodationDetail(roomId: roomId), method: .get, contentType: .json, body: nil) { result in
+        networkHandler.request(endPoint: .accomodationDetail(roomId: roomId), method: .get, contentType: .json, body: nil) { result in
             switch result {
             case .success(let data):
                 guard let decodedData = jsonHandler.convertJsonToObject(from: data, to: RoomDetail.self) else {
