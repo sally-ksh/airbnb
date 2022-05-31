bad77bf44a9e0c64631f089bb80f486a1d65ff80
sally-ksh <sally.devz@gmail.com>
2022-05-31 15:56:34 +0900

    refactor : ReservationDetailResponse 엔티티 제거, 엔티티의 @Getter 제거후 변경

--- BE/airbnb/src/main/java/team07/airbnb/reservation/ReservationDetailResponse.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/ReservationDetailResponse.java
@@ -4,29 +4,25 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import team07.airbnb.address.Address;
 import team07.airbnb.image.Image;
 
 @Getter
 public class ReservationDetailResponse {
      private String roomName;
-
-     private Address address;
-
+     private String address;
      private List<String> images = new ArrayList<>();
-
      private LocalDate startAt;
-
      private LocalDate endAt;
-
      private String hostName;
-
      private int guestAmount;
-
      private int totalPrice;
 
-     public ReservationDetailResponse(String roomName, Address address,
+     @Builder(access = AccessLevel.PRIVATE)
+     public ReservationDetailResponse(String roomName, String address,
          List<String> images, LocalDate startAt, LocalDate endAt, String hostName, int guestAmount,
          int totalPrice) {
           this.roomName = roomName;
@@ -39,21 +35,21 @@ public class ReservationDetailResponse {
           this.totalPrice = totalPrice;
      }
 
-     public static ReservationDetailResponse of(Reservation reservation, List<Image> images) {
-          return new ReservationDetailResponse(
-              reservation.getRoom().getRoomName(),
-              reservation.getRoom().getAddress(),
-              imageEntitiesToStringList(images),
-              reservation.getStartAt(),
-              reservation.getEndAt(),
-              reservation.getRoom().getHost().getUsername(),
-              reservation.getNumberOfGuest(),
-              reservation.getTotalPrice());
+     public static ReservationDetailResponse of(ReservationRoom reservation, List<Image> images) {
+          return ReservationDetailResponse.builder()
+              .roomName(reservation.getTitle())
+              .address(reservation.getAddress())
+              .images(imageEntitiesToStringList(images))
+              .startAt(reservation.checkIn())
+              .endAt(reservation.checkOut())
+              .hostName(reservation.getHostName())
+              .guestAmount(reservation.getNumberOfGuest())
+              .totalPrice(reservation.getTotalPrice())
+              .build();
      }
 
      private static List<String> imageEntitiesToStringList(List<Image> images) {
           return images.stream().map(i -> i.getImageLink())
               .collect(Collectors.toList());
      }
-
 }
