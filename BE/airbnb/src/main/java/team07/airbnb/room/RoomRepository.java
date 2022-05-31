2fc93d1e9e2935f7376f743b098141984899aba4
sally-ksh <sally.devz@gmail.com>
2022-05-31 15:57:20 +0900

    feat: RoomRepository 생성

--- BE/airbnb/src/main/java/team07/airbnb/room/RoomRepository.java
+++ BE/airbnb/src/main/java/team07/airbnb/room/RoomRepository.java
@@ -2,13 +2,13 @@ package team07.airbnb.room;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface RoomRepository extends CrudRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    @Query(value = "select r from Room r join fetch r.host where r.id=:roomId", nativeQuery = false)
+    @Query(value = "select r from Room r join fetch r.host where r.id=:roomId")
     Optional<Room> findRoomWithHostById(@Param("roomId") Long roomId);
+
 }
