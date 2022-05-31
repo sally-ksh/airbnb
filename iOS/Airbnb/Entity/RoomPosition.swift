b7dc3d9ff443ab4b0e8f719c7e71c342ba66fd13
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-31 15:44:05 +0900

    [junu0516/airbnb#26] Ref: RoomPosition 모델에 placeId 속성 추가, 생성자 추가 정의

--- iOS/Airbnb/Entity/RoomPosition.swift
+++ iOS/Airbnb/Entity/RoomPosition.swift
@@ -3,12 +3,22 @@ import Foundation
 
 struct RoomPosition {
     let address: String
+    let placeId: String
     private let category: RoomPositionCategory // 주소가 포함된 시,군,구
     private let latitude: Float // 소수점 14자리
     private let longitude: Float
     
     init(address: String) {
         self.address = address
+        self.placeId = ""
+        self.category = RoomPositionCategory(categoryLiteral: "서울시")
+        self.latitude = 0.0
+        self.longitude = 0.0
+    }
+    
+    init(address: String, placeId: String) {
+        self.address = address
+        self.placeId = placeId
         self.category = RoomPositionCategory(categoryLiteral: "서울시")
         self.latitude = 0.0
         self.longitude = 0.0
