db599eb4a75fc9d7ecd7e8c4c5139a8a7b55ff0c
sally-ksh <sally.devz@gmail.com>
2022-05-31 15:48:31 +0900

    refactor & feat: ReservationService - make() 추가, findOne() 수정

--- BE/airbnb/src/main/java/team07/airbnb/reservation/ReservationService.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/ReservationService.java
@@ -1,24 +1,88 @@
 package team07.airbnb.reservation;
 
 import java.util.List;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import team07.airbnb.image.Image;
 import team07.airbnb.image.ImageRepository;
+import team07.airbnb.room.Room;
+import team07.airbnb.room.RoomService;
+import team07.airbnb.user.User;
+import team07.airbnb.user.UserService;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ReservationService {
+    private final UserService userService;
+    private final RoomService roomService;
+
+    private final ReservationPureRepository reservationPureRepository;
     private final ReservationRepository reservationRepository;
     private final ImageRepository imageRepository;
 
+    /**
+     * 게스트 확인
+     * 숙소 조회 -> 예약 가능여부 및 가격 검증
+     * 예약 저장
+     * @param request
+     */
+    @Transactional
+    public void make(ReservationRequestDto request) {
+        Period period = Period.of(request.getCheckIn(), request.getCheckOut());
+        isValidDate(request, period);
+
+        Room roomInfo = roomService.getRoom(request.getRoomId());
+        ReservationCalculator reservationCalculator = roomInfo.toCalculator();
+        if (reservationCalculator.isNotSame(request.getTotalPrice(), period)) {
+            throw new RuntimeException("cannot make reservation");
+        }
+
+        User guestInfo = userService.get(request.getGuestId());
+        reservationRepository.save(Reservation.of(
+            roomInfo,
+            guestInfo,
+            period.startAt(),
+            period.endAt(),
+            request.getGuestAmount(),
+            request.getTotalPrice()));
+    }
+
     @Transactional(readOnly = true)
     public ReservationDetailResponse findOne(Long reservationId) {
-        Reservation reservationAndRoom = reservationRepository.findReservationWithRoomAndHost(reservationId);
-        List<Image> images = imageRepository.findByRoomId(reservationAndRoom.getRoom().getId());
-        return ReservationDetailResponse.of(reservationAndRoom, images);
+        Reservation reservationAndRoom = reservationPureRepository.findReservationWithRoomAndHost(reservationId);
+        List<Image> images = imageRepository.findByRoomId(reservationAndRoom.reservedNo());
+        return ReservationDetailResponse.of(reservationAndRoom.getInfo(), images);
+    }
+
+    private void isValidDate(ReservationRequestDto request, Period period) {
+        isWithin(request.getRoomId(), period);
+        isInvolved(request.getRoomId(), period);
+    }
+
+    private void isInvolved(Long roomId, Period period) {
+        Optional<Reservation> reservationed = reservationRepository.findFirstByRoomIdAndStartAtBetweenOrEndAtBetween(
+            roomId,
+            period.startAt(), period.endAt(),
+            period.startAt(),  period.endAt());
+
+        if (reservationed.isPresent()) {
+            throw new RuntimeException("make no reservation");
+        }
+    }
+
+    private void isWithin(Long roomId, Period period) {
+        Optional<Reservation> reservationed = reservationRepository.findFirstByRoomIdAndStartAtLessThanEqualAndEndAtGreaterThanEqual(
+            roomId,
+            period.startAt(),
+            period.endAt());
+
+        if (reservationed.isPresent()) {
+            throw new RuntimeException("make no reservation");
+        }
     }
 }
