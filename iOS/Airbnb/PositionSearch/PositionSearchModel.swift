241adea248b9ff971393a0689a3936d0adcba136
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-31 15:46:25 +0900

    [junu0516/airbnb#26] Feat: PositionSearchViewController에서 GooglePlaces API를 통해 장소 검색 자동완성하는 기능 추가
    - SceneDelegate에서 GooglePlaces 사용 위한 key값 적용
    - PositionSearchModel 내부에서 GooglePlaces import 해서 API 요청
    - PositionSearchViewController은 PositionSearchModel의 동작에 맞춰 화면 전환 흐름 수행

--- iOS/Airbnb/PositionSearch/PositionSearchModel.swift
+++ iOS/Airbnb/PositionSearch/PositionSearchModel.swift
@@ -1,9 +1,13 @@
 import Foundation
+import GooglePlaces
 
 class PositionSearchModel {
-    
+
+    private let token = GMSAutocompleteSessionToken()
+    private let client = GMSPlacesClient()
+
     private (set) var isSearching = Observable<Bool>(false)
-    private var filteredSamples = [RoomPosition]()
+    private  (set) var filteredSamples = [RoomPosition]()
     private let categories: [RoomPositionCategory]
     private let samples: [RoomPosition]
     
@@ -11,7 +15,7 @@ class PositionSearchModel {
         self.categories = mockFactory.categories
         self.samples = mockFactory.dataList
     }
-    
+
     func rowsCount() -> Int {
         return isSearching.value ? filteredSamples.count : categories.count
     }
@@ -19,20 +23,48 @@ class PositionSearchModel {
     func titleText(in rowIndex: Int) -> String {
         return isSearching.value ? filteredSamples[rowIndex].address : categories[rowIndex].categoryLiteral
     }
-    
+
     func setIsSearching(_ isSearching: Bool) {
         self.isSearching.value = isSearching
     }
-    
-    func updateSearchResults(searchText: String?) {
-        guard let searchText = searchText else {
-            return
-        }
-        if searchText.isEmpty {
+
+    func updateSearchResults(predictions: [String:String]) {
+        if predictions.isEmpty {
             self.isSearching.value = false
             return
         }
-        self.filteredSamples = samples.filter{ $0.address.contains(searchText) }
+        
+        self.filteredSamples = predictions.map { return RoomPosition(address: $0.key, placeId: $0.value) }
         self.isSearching.value = true
+    }
+    
+    func fetchPredctionList(searchText: String) {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        var predictions: [String:String] = [:]
+        client.findAutocompletePredictions(fromQuery: searchText,
+                                           filter: nil,
+                                           sessionToken: token) { results, error in
+            if error != nil { return }
+            results?.forEach { predictions[$0.attributedPrimaryText.string] = $0.placeID }
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            self?.updateSearchResults(predictions: predictions)
+        }
+    }
+    
+    func searchPositionInfo(index: Int, completion: @escaping (SearchCondition) -> Void) {
+        let positionTitle = self.filteredSamples[index].address
+        let placeId = self.filteredSamples[index].placeId
+        client.lookUpPlaceID(placeId) { result,error in
+            if error != nil { return }
+            if let coordinate = result?.coordinate {
+                let searchCondition = SearchCondition(positionTitle: positionTitle, logntitude: coordinate.longitude.magnitude, latitude: coordinate.latitude.magnitude)
+                completion(searchCondition)
+            }
+        }
     }
 }
