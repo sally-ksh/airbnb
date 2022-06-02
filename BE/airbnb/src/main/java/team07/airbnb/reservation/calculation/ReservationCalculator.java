84b05d8a82d163351a0020e8346349a58b78b9c6
sally-ksh <sally.devz@gmail.com>
2022-06-03 02:39:13 +0900

    refactor : ReservationCalculator - ReservationFeeAndTax,  isValid()

--- BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationCalculator.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationCalculator.java
@@ -5,35 +5,38 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
 import lombok.RequiredArgsConstructor;
-import team07.airbnb.reservation.Period;
 
 @RequiredArgsConstructor
 @Component
 public class ReservationCalculator {
 	private final WeeklyDiscountPolicy weeklyDiscountPolicy;
 	private final RoomCleaningFee roomCleaningFee;
-	private final ReservationFee reservationFee;
-	private final ReservationTax reservationTax;
+	private final ReservationFeeAndTax reservationFeeAndTax;
 	private ReservationReport reservationReport;
 
 	public void add(ReservationReport reservationReport) {
 		this.reservationReport = reservationReport;
 	}
 
-	public boolean isNotSame(int totalPrice, Period period) {
-		int calculate = this.calculate(period);
+	public void isValid(int totalPrice) {
+		if (this.isNotSame(totalPrice)) {
+			throw new RuntimeException("요청 예약 금액이 유효하지 않습니다.");
+		}
+	}
+
+	private boolean isNotSame(int totalPrice) {
+		int calculate = this.calculate();
 		return calculate != totalPrice;
 	}
 
-	private int calculate(Period period) {
+	private int calculate() {
 		BigDecimal weeklyDiscounted = this.weeklyDiscountPolicy.getDiscounted(this.reservationReport);
 		BigDecimal totalPrice = this.reservationReport.getMinus(weeklyDiscounted);
 
 		BigDecimal cleaningFee = roomCleaningFee.apply(totalPrice);
-		BigDecimal fee = reservationFee.apply(totalPrice);
-		BigDecimal tax = reservationTax.apply(fee);
+		BigDecimal feeAndTax = reservationFeeAndTax.apply(totalPrice);
 		// 총 금액
-		BigDecimal payment = totalPrice.subtract(cleaningFee).subtract(fee).subtract(tax);
+		BigDecimal payment = totalPrice.subtract(cleaningFee).subtract(feeAndTax);
 		return payment.intValueExact();
 	}
 }
