8c11c97b620ee90946f0815ca555e22477c49085
sally-ksh <sally.devz@gmail.com>
2022-06-03 03:41:52 +0900

    feat : SearchDto - RoomsResponse

--- BE/airbnb/src/main/java/team07/airbnb/search/SearchDto.java
+++ BE/airbnb/src/main/java/team07/airbnb/search/SearchDto.java
@@ -1,6 +1,8 @@
 package team07.airbnb.search;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -45,5 +47,38 @@ public class SearchDto {
 	}
 
 	public static class RoomsResponse {
+		private final int numberOfRooms;
+		private final List<RoomCard> rooms = new ArrayList<>();
+
+		public RoomsResponse(int numberOfRooms) {
+			this.numberOfRooms = numberOfRooms;
+		}
+	}
+
+	public static class RoomCard {
+		private final Long roomId;
+		private final String roomName;
+		private final int price;
+		private final int totalPrice;
+		private final String thumbnailImage;
+		private final String averageOfStar;
+		private final int numberOfReviews;
+		private final boolean isWished;
+		private final String latitude;
+		private final String logitude;
+
+		public RoomCard(Long roomId, String roomName, int price, int totalPrice, String thumbnailImage,
+			String averageOfStar, int numberOfReviews, boolean isWished, String latitude, String logitude) {
+			this.roomId = roomId;
+			this.roomName = roomName;
+			this.price = price;
+			this.totalPrice = totalPrice;
+			this.thumbnailImage = thumbnailImage;
+			this.averageOfStar = averageOfStar;
+			this.numberOfReviews = numberOfReviews;
+			this.isWished = isWished;
+			this.latitude = latitude;
+			this.logitude = logitude;
+		}
 	}
 }
