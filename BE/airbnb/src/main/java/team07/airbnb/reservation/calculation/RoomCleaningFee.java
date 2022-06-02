fa4dd712f238ee74476dd2b308ae03ba16d5cae4
sally-ksh <sally.devz@gmail.com>
2022-06-02 16:27:30 +0900

    refactor : create WeeklyDiscountPolicy

---
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/RoomCleaningFee.java
@@ -0,0 +1,14 @@
+package team07.airbnb.reservation.calculation;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class RoomCleaningFee {
+	private static final BigDecimal ratioOfCleaningFee = new BigDecimal("0.02");
+
+	public BigDecimal apply(BigDecimal totalPrice) {
+		return totalPrice.multiply(this.ratioOfCleaningFee);
+	}
+}
