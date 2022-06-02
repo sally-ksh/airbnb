58cfea12aca8b83281d0ecb6a84ae75878a5ec99
sally-ksh <sally.devz@gmail.com>
2022-06-02 16:27:03 +0900

    refactor : create ReservationTax

---
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationTax.java
@@ -0,0 +1,14 @@
+package team07.airbnb.reservation.calculation;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class ReservationTax {
+	private static final BigDecimal taxRate = new BigDecimal("0.1");
+
+	public BigDecimal apply(BigDecimal totalPrice) {
+		return totalPrice.multiply(this.taxRate);
+	}
+}
