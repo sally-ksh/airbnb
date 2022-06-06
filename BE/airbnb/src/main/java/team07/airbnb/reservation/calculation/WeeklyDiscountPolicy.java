e005fea0b729cad6df3b27835ded88a7af271684
sally-ksh <sally.devz@gmail.com>
2022-06-07 07:07:41 +0900

    refactor : WeeklyDiscountPolicy - implement DiscountPolicy

--- BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/WeeklyDiscountPolicy.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/WeeklyDiscountPolicy.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
 @Component
-public class WeeklyDiscountPolicy {
-	private static final int discountConditionDays = 7;
+public class WeeklyDiscountPolicy extends DiscountPolicy{
 	public static final BigDecimal NONE_DISCOUNTED_AMOUNT = BigDecimal.ZERO;
+	private static final int discountConditionDays = 7;
 	private static final BigDecimal weeklyDiscountRate = new BigDecimal("0.04");
 
+	public WeeklyDiscountPolicy(FeeOrTax... feeOrTaxes) {
+		super(feeOrTaxes);
+	}
+
+	@Override
 	public BigDecimal getDiscounted(ReservationReport reservationReport) {
 		if (reservationReport.appliedPeriod(discountConditionDays)) {
 			return reservationReport.getPriceOfRoom().multiply(weeklyDiscountRate);
