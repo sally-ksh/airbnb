bfa96be8949b5b015aa45f7f34967868675846d4
sally-ksh <sally.devz@gmail.com>
2022-06-07 07:08:31 +0900

    refactor : ReservationFeeAndTax - implement FeeOrTax

--- BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationFeeAndTax.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationFeeAndTax.java
@@ -1,16 +1,18 @@
 package team07.airbnb.reservation.calculation;
 
+import static team07.airbnb.reservation.calculation.ReservationFee.FEE_RATE;
+
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
-public class ReservationFeeAndTax extends ReservationFee{
+public class ReservationFeeAndTax implements FeeOrTax{
 	private static final BigDecimal TAX_RATE = new BigDecimal("0.1");
 
 	@Override
-	BigDecimal apply(BigDecimal totalPrice) {
-		BigDecimal fee = totalPrice.multiply(FEE_RATE);
+	public BigDecimal calculate(BigDecimal totalAmount) {
+		BigDecimal fee = totalAmount.multiply(FEE_RATE);
 		BigDecimal tax = fee.multiply(TAX_RATE);
 		return fee.add(tax);
 	}
