c14d1d4036cca1079b50ef3c60759f22f77da4b6
sally-ksh <sally.devz@gmail.com>
2022-06-07 07:12:10 +0900

    refactor : ReservationCalculator with DiscountPolicy field

--- BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationCalculator.java
+++ BE/airbnb/src/main/java/team07/airbnb/reservation/calculation/ReservationCalculator.java
@@ -2,41 +2,21 @@ package team07.airbnb.reservation.calculation;
 
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class ReservationCalculator {
-	private final WeeklyDiscountPolicy weeklyDiscountPolicy;
-	private final RoomCleaningFee roomCleaningFee;
-	private final ReservationFeeAndTax reservationFeeAndTax;
-	private ReservationReport reservationReport;
+	private final DiscountPolicy discountPolicy;
 
-	public void add(ReservationReport reservationReport) {
-		this.reservationReport = reservationReport;
-	}
-
-	public void isValid(int totalPrice) {
-		if (this.isNotSame(totalPrice)) {
+	public void isValid(int totalPrice, ReservationReport reservationReport) {
+		if (this.isNotSame(totalPrice, reservationReport)) {
 			throw new RuntimeException("요청 예약 금액이 유효하지 않습니다.");
 		}
 	}
 
-	private boolean isNotSame(int totalPrice) {
-		int calculate = this.calculate();
+	private boolean isNotSame(int totalPrice, ReservationReport reservationReport) {
+		int calculate = discountPolicy.settle(reservationReport).intValue();
 		return calculate != totalPrice;
-	}
-
-	private int calculate() {
-		BigDecimal weeklyDiscounted = this.weeklyDiscountPolicy.getDiscounted(this.reservationReport);
-		BigDecimal totalPrice = this.reservationReport.getMinus(weeklyDiscounted);
-
-		BigDecimal cleaningFee = roomCleaningFee.apply(totalPrice);
-		BigDecimal feeAndTax = reservationFeeAndTax.apply(totalPrice);
-		// 총 금액
-		BigDecimal payment = totalPrice.subtract(cleaningFee).subtract(feeAndTax);
-		return payment.intValueExact();
 	}
 }
