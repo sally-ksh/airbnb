abe5060f34c06fb779a050ded151b2649010b630
leezzangmin <64303390+leezzangmin@users.noreply.github.com>
2022-06-07 17:53:30 +0900

    Update SearchApiController.java

--- BE/airbnb/src/main/java/team07/airbnb/search/SearchApiController.java
+++ BE/airbnb/src/main/java/team07/airbnb/search/SearchApiController.java
@@ -1,7 +1,7 @@
 package team07.airbnb.search;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
