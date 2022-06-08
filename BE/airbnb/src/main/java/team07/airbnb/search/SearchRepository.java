ad55bc99a8928a8f2d99a53c8522642e3cdc7417
sally-ksh <sally.devz@gmail.com>
2022-06-08 16:15:46 +0900

    feat : SearchRepository - findByReservationDate()

--- BE/airbnb/src/main/java/team07/airbnb/search/SearchRepository.java
+++ BE/airbnb/src/main/java/team07/airbnb/search/SearchRepository.java
@@ -2,6 +2,10 @@ package team07.airbnb.search;
 
 import java.util.List;
 
+import team07.airbnb.reservation.Period;
+
 public interface SearchRepository {
-	List<SearchRoomDto> searchLocation(SearchParam searchParam);
+	List<SearchRoomDto> searchLocation(SearchParam searchParam, List<Long> reservationList);
+
+	List<SearchReservationRoomDto> findByReservationDate(Period period);
 }
