512084ebbab15a84a33d66340ded112550844808
sally-ksh <sally.devz@gmail.com>
2022-06-08 16:12:44 +0900

    feat : GET 요청 파라미터 queryString로 대체

--- BE/airbnb/src/main/java/team07/airbnb/search/SearchApiController.java
+++ BE/airbnb/src/main/java/team07/airbnb/search/SearchApiController.java
@@ -1,12 +1,14 @@
 package team07.airbnb.search;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import java.util.HashMap;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,9 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class SearchApiController {
 	private final SearchService searchService;
 
+	// ?location="강원"&checkinDate=""&checkoutDate=""&minPrice=0&maxPrice=0&guestAmount=0
 	@GetMapping
-	public ResponseEntity searchRegion(@Valid @RequestBody SearchDto.LocationRequest request) {
-		SearchDto.RoomsResponse response = searchService.findRoomsOfRegion(request);
+	public ResponseEntity<SearchDto.RoomsResponse> searchRegion(@RequestParam HashMap<String, Object> request) {
+		SearchDto.RoomsResponse response = searchService.findRoomsOfRegion(SearchDto.LocationRequest.of(request));
 		return ResponseEntity.ok().body(response);
 	}
 }
